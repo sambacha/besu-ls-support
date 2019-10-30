1c042bd0450967e3442011113208b3df3b077be0
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2019-10-30 14:04:37 +0100

    add functions

--- boostrap-ibft-network.sh
+++ boostrap-ibft-network.sh
@@ -6,12 +6,12 @@ source common/functions
 sh scripts/generate-ibft-network-files 3 4
 sh scripts/run-ibft-network 3 4
 
-sleep 5s
-echo "................................"
-echo "net_peerCount:"
-curl -X POST --data '{"jsonrpc":"2.0","method":"net_peerCount","params":[],"id":1}' localhost:8545
-echo
-echo "................................"
+#sleep 5s
+#echo "................................"
+#echo "net_peerCount:"
+#curl -X POST --data '{"jsonrpc":"2.0","method":"net_peerCount","params":[],"id":1}' localhost:8545
+#echo
+#echo "................................"
 
 echo "................................"
 echo "Network topology:"
