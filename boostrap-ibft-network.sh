c230a3cc14cdc0299797c8ff02e4e126b268406b
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2019-10-30 13:50:46 +0100

    Add network topology file

--- boostrap-ibft-network.sh
+++ boostrap-ibft-network.sh
@@ -6,6 +6,14 @@ source common/functions
 sh scripts/generate-ibft-network-files 3 4
 sh scripts/run-ibft-network 3 4
 
-sleep 10s
+sleep 5s
+echo "................................"
 echo "net_peerCount:"
 curl -X POST --data '{"jsonrpc":"2.0","method":"net_peerCount","params":[],"id":1}' localhost:8545
+echo
+echo "................................"
+
+echo "................................"
+echo "Network topology:"
+cat out/network-topology
+echo "................................"
