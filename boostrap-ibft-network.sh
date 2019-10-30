46a91b4aa795d50cdf48252a22c186519141dc86
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2019-10-30 13:31:41 +0100

    add net peer count

--- boostrap-ibft-network.sh
+++ boostrap-ibft-network.sh
@@ -4,3 +4,8 @@ source common/variables
 source common/functions
 
 sh scripts/generate-ibft-network-files 3 4
+sh scripts/run-ibft-network 3 4
+
+sleep 10s
+echo "net_peerCount:"
+curl -X POST --data '{"jsonrpc":"2.0","method":"net_peerCount","params":[],"id":1}' localhost:8545
