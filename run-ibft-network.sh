4f9c753d1e4e08a79c3b34a9b943451be035e6ff
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2019-10-30 11:57:01 +0100

    Update run-ibft-network.sh

--- run-ibft-network.sh
+++ run-ibft-network.sh
@@ -10,7 +10,5 @@ echo "Starting bootnode 1."
 bootnode1LogFile="out/ibft-network/bootnode-1/log"
 $BESU_PATH --data-path=out/ibft-network/bootnode-1/data --genesis-file=out/ibft-network/genesis.json --rpc-http-enabled --rpc-http-api=ETH,NET,IBFT --host-whitelist=* --rpc-http-cors-origins=all > $bootnode1LogFile &
 sleep 5s
-#cat out/ibft-network/bootnode-1/log | grep "Enode URL"
-enodeURLLogString=$(cat $bootnode1LogFile | grep --text "Enode URL")
-#echo $enodeURLLogString
-echo $enodeURLLogString | awk '{print $NF}'
+bootnode1EnodeURL=$(cat $bootnode1LogFile | grep --text "Enode URL" | awk '{print $NF}')
+echo "Bootnode 1 enode URL: $bootnode1EnodeURL"
